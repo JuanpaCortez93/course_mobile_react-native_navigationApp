2b125c5702934e0ab474daa15b8519038c7e2e68
Juan Pablo Cortez Mosquera <63519906+JuanpaCortez93@users.noreply.github.com>
2026-04-02 21:52:13 -0500

    feat: nativewind added

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View className="mt-10">
+      <Text className="text-3xl font-bold text-blue-500">Hello world!</Text>
+    </View>
+  );
+};
+
+export default App;
