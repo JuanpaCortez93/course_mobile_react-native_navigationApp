580c65f8d0aedd0e7b8e134d332af2372cec0c65
Juan Pablo Cortez Mosquera <63519906+JuanpaCortez93@users.noreply.github.com>
2026-04-02 22:11:01 -0500

    feat: fonts added

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <View className="mt-10">
-      <Text className="text-3xl font-bold text-blue-500">Hello world!</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-6 mx-2.5">
+        <Text style={{ fontFamily: "WorkSans-Black" }}>Hello world!</Text>
+
+        <Text className="font-work-black">Hello world!</Text>
+        <Text className="font-work-light">Hello world!</Text>
+        <Text className="font-work-medium">Hello world!</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
